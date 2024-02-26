d8ed2727b71510a3df8496363e2256e0abcd94b4
SameerGiri69 <sameergiri153000@gmail.com>
2024-02-26 07:58:21 +0545

    Token Generator and User Login

---
+++ ViewModels/NewUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_EF_Core.ViewModels
+{
+    public class NewUserViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
